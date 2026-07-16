5977c1b73485303a9544397333cfa863797bb072
mamoorali <mamoorali3may@gmail.com>
2026-07-16 05:39:56 +0500

    Stage 2: read endpoints with 404

--- BackendEngineering/task-1-CRUD/server.js
+++ BackendEngineering/task-1-CRUD/server.js
@@ -2,6 +2,40 @@ const express = require('express');
 const app = express();
 port = 3000;
 
+const tasks = [
+    {
+        id: 1,
+        title: "Learn Express",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Finish Backend Task",
+        done: false
+    },
+    {
+        id: 3,
+        title: "Push to GitHub",
+        done: true
+    }
+];
+
+app.get('/tasks', (req,res) =>{
+    res.send(tasks);
+});
+app.get("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find((t) => t.id === id);
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+    else{
+    res.json(task)
+    };
+});
+
 app.get('/', (req,res) =>
 {
     res.json({
@@ -17,10 +51,6 @@ app.get('/health', (req,res) =>
         "status": "ok"    
     });
 });
-
-
-
-
 
 app.listen(port, () =>
 {
